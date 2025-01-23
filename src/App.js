df58fe0b783900ef1b9a7fe025e01b119f146522
TeaMarusic <tea.marusic@fsre.sum.ba>
2025-01-23 22:14:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Logo from "./components/logo";
+import Form from "./components/form";
+import PackingList from "./components/packinglist";
+import Stats from "./components/Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItems={handleDeleteItem}
+        onPackedItem={handleToggleItem}
+        deleteAll={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
